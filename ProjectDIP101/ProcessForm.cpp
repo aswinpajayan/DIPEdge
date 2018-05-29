96814a1588263e7742e167289089ecffc53e7dfe
aswinp2008 <aswinp2008@gmail.com>
2018-05-30 00:36:06 +0530

    Initial config done

---
+++ ProjectDIP101/ProcessForm.cpp
@@ -0,0 +1,12 @@
+#include "ProcessForm.h"
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+
+void Main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	ProjectDIP101::ProcessForm form;
+	Application::Run(%form);
+}
